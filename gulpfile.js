9b0bda21899232027807aaefd6ebec399d7578d1
NokaK <nokakipiani93@gmail.com>
2018-12-17 19:27:50 +0400

    add gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,21 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false
+  }))
     .pipe(gulp.dest('styles'));
 });
  
 gulp.task('watch', function () {
-  gulp.watch('scss/style.scss', gulp.parallel('sass'));
+  gulp.watch('scss/*.scss', gulp.parallel('sass'));
 });
